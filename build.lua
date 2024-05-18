4e66a2240996de602ae96aeccd1e8f52e7071376
Joona Gynther <joona@gynther.xyz>
2024-05-18 23:41:06 +0300

    separately build and link source files

--- build.lua
+++ build.lua
@@ -4,8 +4,7 @@ local config = ninja.init()
 
 -- Add basic compile flags
 config:add_variable("compiler_flags", "-Wall -Wextra -O2 -std=c++2b")
-config:add_variable("lib", "-lraylib -lluajit-5.1")
-config:add_rule("clang", "clang++ $compiler_flags $lib $in -o $out")
+config:add_rule("clang", "clang++ $compiler_flags -c $in -o $out")
 
 -- Lua modules
 config:add_variable("module_flags", "-shared -fPIC")
@@ -16,12 +15,31 @@ local modules = ninja.utils.files_with_prefix("lua_", "tools")
 
 for _, file in ipairs(modules) do
     local filename = ninja.utils.filename(file)
-    local out = string.sub(filename, 5, -4) -- files start with lua_ and end in .cc
-    config:add_build("tools/" .. out .. ".so", "build_lua_module", file)
+    local out = string.sub(filename, 5, -4) .. ".so" -- files start with lua_ and end in .cc
+    config:add_build("tools/" .. out, "build_lua_module", file)
 end
 
+-- Build source files
+local sources = ninja.utils.files_with_suffix(".cc", "src")
+
+local targets = {}
+
+for _, file in ipairs(sources) do
+    local filename = ninja.utils.filename(file)
+    local out = "build/" .. string.sub(filename, 0, -4) .. ".o"
+
+    table.insert(targets, out)
+
+    config:add_build(out, "clang", file)
+end
+
+-- Link object files
+config:add_variable("lib", "-lraylib -lluajit-5.1")
+config:add_rule("link", "clang++ $lib $in -o $out")
+config:add_build("shard", "link", table.concat(targets, " "))
+
 -- Build main.cc
-config:add_build("build/main", "clang", "main.cc")
+-- config:add_build("build/main", "clang", "main.cc")
 
 -- Generate build.ninja
 config:write_buildfile(config:generate())
